d35142e6be8d2c4bca90a90fd27bd9d4e53266de
raoul <raoul@waag.org>
2011-12-27 15:35:25 +0100

    Remove empire dependencies

--- waag.artsholland.api/src/org/waag/artsholland/api/domain/Profile.java
+++ waag.artsholland.api/src/org/waag/artsholland/api/domain/Profile.java
@@ -1,20 +1,15 @@
 package org.waag.artsholland.api.domain;
 
-import java.net.URISyntaxException;
 import java.net.URL;
 
 import javax.persistence.Entity;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import com.clarkparsia.empire.SupportsRdfId;
-import com.clarkparsia.empire.annotation.Namespaces;
-import com.clarkparsia.empire.annotation.RdfsClass;
-
 @Entity
-@Namespaces({"foaf", "http://xmlns.com/foaf/0.1/"})
-@RdfsClass("foaf:Person")
+//@Namespaces({"foaf", "http://xmlns.com/foaf/0.1/"})
+//@RdfsClass("foaf:Person")
 @XmlRootElement(name="profile")
-public class Profile implements SupportsRdfId, org.waag.artsholland.model.Profile {
+public class Profile implements /*SupportsRdfId,*/ org.waag.artsholland.model.Profile {
 	private String name;
 	private String email;
 	private URL url;
@@ -44,18 +39,18 @@ public class Profile implements SupportsRdfId, org.waag.artsholland.model.Profil
 		this.email = email;
 	}
 
-	@Override
-	public RdfKey getRdfId() {
-		try {
-			return new URIKey(getUrl().toURI());
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+//	@Override
+//	public RdfKey getRdfId() {
+//		try {
+//			return new URIKey(getUrl().toURI());
+//		} catch (URISyntaxException e) {
+//			e.printStackTrace();
+//		}
+//		return null;
+//	}
 
-	@Override
-	public void setRdfId(RdfKey theId) {
-		
-	}
+//	@Override
+//	public void setRdfId(RdfKey theId) {
+//		
+//	}
 }
