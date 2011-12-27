bbc53d93fae9ca171cae723685cdf06025e7229c
raoul <raoul@waag.org>
2011-12-27 15:30:21 +0100

    Remove empire dependencies

--- waag.artsholland.api/src/org/waag/artsholland/api/EmpireFactoryBean.java
+++ waag.artsholland.api/src/org/waag/artsholland/api/EmpireFactoryBean.java
@@ -1,32 +1,32 @@
-package org.waag.artsholland.api;
-
-import java.util.Map;
-
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.spi.PersistenceProvider;
-
-import org.springframework.beans.factory.FactoryBean;
-
-import com.clarkparsia.empire.Empire;
-import com.clarkparsia.empire.sesametwo.OpenRdfEmpireModule;
-
-public class EmpireFactoryBean implements FactoryBean<EntityManagerFactory> {
-	private Map<String, String> config; 
-	public Map<String, String> getConfig() {
-		return config;
-	}
-	public void setConfig(Map<String, String> config) {
-		this.config = config;
-	}
-	public EntityManagerFactory getObject() throws Exception {
-		Empire.init(new OpenRdfEmpireModule());
-		PersistenceProvider aProvider = Empire.get().persistenceProvider();
-		return aProvider.createEntityManagerFactory("", config); //.createEntityManager();
-	}
-	public Class<?> getObjectType() {
-		return EntityManagerFactory.class;
-	}
-	public boolean isSingleton() {
-		return true;
-	}
-}
+//package org.waag.artsholland.api;
+//
+//import java.util.Map;
+//
+//import javax.persistence.EntityManagerFactory;
+//import javax.persistence.spi.PersistenceProvider;
+//
+//import org.springframework.beans.factory.FactoryBean;
+//
+//import com.clarkparsia.empire.Empire;
+//import com.clarkparsia.empire.sesametwo.OpenRdfEmpireModule;
+//
+//public class EmpireFactoryBean implements FactoryBean<EntityManagerFactory> {
+//	private Map<String, String> config; 
+//	public Map<String, String> getConfig() {
+//		return config;
+//	}
+//	public void setConfig(Map<String, String> config) {
+//		this.config = config;
+//	}
+//	public EntityManagerFactory getObject() throws Exception {
+//		Empire.init(new OpenRdfEmpireModule());
+//		PersistenceProvider aProvider = Empire.get().persistenceProvider();
+//		return aProvider.createEntityManagerFactory("", config); //.createEntityManager();
+//	}
+//	public Class<?> getObjectType() {
+//		return EntityManagerFactory.class;
+//	}
+//	public boolean isSingleton() {
+//		return true;
+//	}
+//}
