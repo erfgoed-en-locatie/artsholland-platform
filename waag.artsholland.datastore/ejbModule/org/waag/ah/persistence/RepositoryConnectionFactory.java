283c377d9c4457ca76d348a567e3d1075b35a932
raoul <raoul@waag.org>
2012-01-23 14:50:46 +0100

    Added datastore project

---
+++ waag.artsholland.datastore/ejbModule/org/waag/ah/persistence/RepositoryConnectionFactory.java
@@ -0,0 +1,14 @@
+package org.waag.ah.persistence;
+
+import java.io.IOException;
+
+import javax.ejb.Local;
+
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+
+@Local
+public interface RepositoryConnectionFactory {
+	public RepositoryConnection getConnection() 
+			throws IOException, RepositoryException;
+}
---
+++ waag.artsholland.datastore/ejbModule/org/waag/ah/persistence/SAILConnectionFactory.java
@@ -0,0 +1,78 @@
+package org.waag.ah.persistence;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.jboss.ejb3.annotation.LocalBinding;
+import org.jboss.ejb3.annotation.Management;
+import org.jboss.ejb3.annotation.Service;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.sail.SailRepositoryConnection;
+import org.waag.ah.ServiceManagement;
+
+import com.bigdata.rdf.sail.BigdataSail;
+import com.bigdata.rdf.sail.BigdataSailRepository;
+import com.bigdata.rdf.sail.BigdataSailRepositoryConnection;
+
+@Service(objectName = SAILConnectionFactory.OBJECT_NAME)
+@Management(ServiceManagement.class)
+@LocalBinding(jndiBinding = "java:global/"+SAILConnectionFactory.OBJECT_NAME)  
+public class SAILConnectionFactory extends AbstractConnectionFactory  
+		implements ServiceManagement, RepositoryConnectionFactory {
+	private Logger logger = Logger.getLogger(SAILConnectionFactory.class);
+	private static BigdataSailRepository repo = null;
+//	private static final String PROPERTY_FILE = "META-INF/bigdata_quads.properties";
+//	private static final String PROPERTY_FILE = "META-INF/bigdata_fullfeature.properties";
+	private static final String PROPERTY_FILE = "META-INF/bigdata.properties";
+	
+	public final static String OBJECT_NAME = "artsholland:service=SAILConnectionFactory";
+
+	@Override
+	public void create() throws IOException, RepositoryException {
+		logger.info("Creating service "+OBJECT_NAME);
+		Properties properties = loadProperties(PROPERTY_FILE);
+		repo = createRepository(properties);
+		repo.initialize();
+	}
+	
+	public SailRepositoryConnection getConnection() throws RepositoryException { 
+//		if (repo == null) {
+//			synchronized (this) {
+//				if (repo == null) {
+//
+//				}
+//			}//com.bigdata.rdf.sail.isolatableIndices
+//		}
+		BigdataSailRepositoryConnection conn = 
+				(BigdataSailRepositoryConnection) repo.getReadWriteConnection();
+		conn.setAutoCommit(false);
+		logger.info("Created connection: "+conn);
+		return conn;
+    }
+	
+	@Override
+	protected BigdataSailRepository createRepository(Properties properties) {
+		BigdataSail sail = new BigdataSail(properties);
+		return new BigdataSailRepository(sail);		
+	}
+
+//	@Override
+	public void close() {
+		logger.info("Closing repository "+repo);
+		try {
+			if (repo != null) {
+				repo.shutDown();
+			}
+		} catch (RepositoryException e) {
+			logger.warn(e.getMessage());
+		}
+	}	
+	
+//	@PreDestroy
+	@Override
+	public void destroy() {
+		logger.info("Destroying service "+this);
+		close();
+	}
+}
