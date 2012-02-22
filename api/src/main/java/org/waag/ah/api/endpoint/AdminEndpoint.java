fde7b80c40bd90a3a91b0839bc51f9a3b85b5e70
raoul <raoul@waag.org>
2012-02-22 18:10:49 +0100

    Refactored project to Maven layout

---
+++ api/src/main/java/org/waag/ah/api/endpoint/AdminEndpoint.java
@@ -0,0 +1,45 @@
+package org.waag.ah.api.endpoint;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.w3c.dom.Element;
+import org.waag.ah.ApiService;
+import org.waag.ah.api.ApplicationListResponse;
+import org.waag.ah.api.ApplicationType;
+import org.waag.ah.api.ObjectFactory;
+import org.waag.ah.api.service.AdminService;
+import org.waag.ah.model.Application;
+
+@Endpoint
+public class AdminEndpoint implements ApiService {
+	final static Logger logger = LoggerFactory.getLogger(AdminEndpoint.class);
+	private static final String NAMESPACE_URI = "http://artsholland.com/schema";
+	private final AdminService adminService;
+	private ObjectFactory objectFactory;
+	
+	@Autowired
+	public AdminEndpoint(AdminService adminService) {
+		this.adminService = adminService;
+		this.objectFactory = new ObjectFactory();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@PayloadRoot(namespace=NAMESPACE_URI, localPart="ApplicationListRequest")                        
+	public @ResponsePayload ApplicationListResponse getApplicationList(
+			@RequestPayload Element appElement) {                              
+		List<Application> result = adminService.getApplicationList();
+		ApplicationListResponse response = 
+				objectFactory.createApplicationListResponse();
+//		response.getApplication().addAll(result);
+//				(Collection<? extends ApplicationType>) result);
+		return response;
+	}
+}
