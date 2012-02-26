28a2f63abc716547f1e07dcaf58acb91bd82b120
raoul <raoul@waag.org>
2012-02-26 12:51:38 +0100

    Fixed webapp context root

--- scheduler/src/main/java/org/waag/ah/service/scheduler/SchedulerServiceBean.java
+++ scheduler/src/main/java/org/waag/ah/service/scheduler/SchedulerServiceBean.java
@@ -82,6 +82,7 @@ public class SchedulerServiceBean implements MessageListener {
 			};
 			
 			for (String sourceURL: sourceURLs) {
+				logger.info("Scheduling URL for import: "+sourceURL);
 				TextMessage msg = session.createTextMessage(sourceURL);
 				sender.send(msg);
 				logger.info("Message sent successfully to import queue");
