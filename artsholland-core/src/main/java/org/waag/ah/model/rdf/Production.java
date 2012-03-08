f4eea829a1d618d56092436fe232f23e8b1f3e14
Bert Spaan <bert@waag.org>
2012-03-08 16:42:14 +0100

    Vergat VenueImpl.java toe te voegen. En aanpassingen aan xsparql

--- artsholland-core/src/main/java/org/waag/ah/model/rdf/Production.java
+++ artsholland-core/src/main/java/org/waag/ah/model/rdf/Production.java
@@ -3,8 +3,11 @@ package org.waag.ah.model.rdf;
 import org.codehaus.jackson.annotate.JsonAutoDetect;
 import org.codehaus.jackson.annotate.JsonAutoDetect.Visibility;
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 
 @JsonAutoDetect(fieldVisibility=Visibility.NONE, getterVisibility=Visibility.NONE, isGetterVisibility=Visibility.NONE, creatorVisibility=Visibility.NONE, setterVisibility=Visibility.NONE)
+@JsonSerialize(include=Inclusion.NON_EMPTY)
 public interface Production extends org.openrdf.repository.object.RDFObject {
 	
 	@JsonProperty
---
+++ artsholland-core/src/main/java/org/waag/ah/model/rdf/VenueImpl.java
@@ -0,0 +1,74 @@
+
+package org.waag.ah.model.rdf;
+
+import java.math.BigDecimal;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.codehaus.jackson.annotate.JsonAutoDetect;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.annotate.JsonAutoDetect.Visibility;
+import org.openrdf.annotations.Iri;
+import org.openrdf.repository.object.LangString;
+import org.openrdf.repository.object.util.GenericType;
+
+@Iri(RDFObject.ah + "Venue")
+@JsonAutoDetect(fieldVisibility=Visibility.NONE, getterVisibility=Visibility.NONE, isGetterVisibility=Visibility.NONE, creatorVisibility=Visibility.NONE, setterVisibility=Visibility.NONE)
+public abstract class VenueImpl extends RDFObject implements Venue {
+
+	@Iri(ah + "cidn")
+	private String cidn;
+	
+	@Iri(dc + "title")
+	private Set<LangString> titles;
+	
+	@Iri(dc + "description")
+	private Set<LangString> descriptions;
+	
+	@Iri(ah + "room")
+	private Set<Room> rooms;
+	
+	@Iri(geo + "lat")
+	private BigDecimal latitude;
+	
+	@Iri(geo + "long")
+	private BigDecimal longitude;
+	
+	@Override
+	public String getTitle() {
+		for (LangString title: titles) {
+			return title.toString();
+		}
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		for (LangString title: titles) {
+			return title.toString();
+		}
+		return "";
+	}
+
+	@Override
+	public Set<Room> getRooms() {
+		return rooms;
+	}
+	
+	@Override
+	public String getCidn() {
+		return cidn;
+	}
+
+	@Override	
+	public BigDecimal getLatitude() {
+		return latitude;
+	}
+	
+	@Override
+	public BigDecimal getLongitude() {
+		return longitude;
+	}
+
+	
+}
