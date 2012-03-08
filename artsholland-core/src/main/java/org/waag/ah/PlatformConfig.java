405706d0cfc8efc61069ced76f04d94e133e994d
raoul <raoul@waag.org>
2012-03-08 15:14:03 +0100

    Updated bigdata JAR

--- artsholland-core/src/main/java/org/waag/ah/PlatformConfig.java
+++ artsholland-core/src/main/java/org/waag/ah/PlatformConfig.java
@@ -5,12 +5,24 @@ import org.apache.commons.configuration.PropertiesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Platform configuration helper class.
+ *  
+ * @author Raoul Wissink <raoul@raoul.net>
+ */
 public class PlatformConfig extends PropertiesConfiguration {
 	final static Logger logger = LoggerFactory.getLogger(PlatformConfig.class);
 	private static PropertiesConfiguration platformConfig;
 	
 	private PlatformConfig() {}
 	
+	/**
+	 * Return platform configuration instance.
+	 * 
+	 * @throws ConfigurationException
+	 * @author	Raoul Wissink <raoul@raoul.net>
+	 * @since	Mar 8, 2012
+	 */
 	public synchronized static PropertiesConfiguration getConfig() 
 			throws ConfigurationException {
 		if (platformConfig == null) {
