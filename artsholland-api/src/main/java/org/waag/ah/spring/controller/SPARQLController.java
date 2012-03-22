d240f098b8e838b7b87f5c4a6e934d1b6a13c8be
Bert Spaan <bert.spaan@gmail.com>
2012-03-22 14:12:26 +0100

    Added API key authentication to SPARQLController and SNORQL scripts.

--- artsholland-api/src/main/java/org/waag/ah/spring/controller/SPARQLController.java
+++ artsholland-api/src/main/java/org/waag/ah/spring/controller/SPARQLController.java
@@ -1,6 +1,7 @@
 package org.waag.ah.spring.controller;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.concurrent.ExecutionException;
 
 import javax.annotation.Resource;
@@ -9,13 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.waag.ah.spring.service.SPARQLService;
 
-//@Secured("ROLE_API_USER")
+@Secured("ROLE_API_USER")
 @Controller
 public class SPARQLController {
 	@SuppressWarnings("unused")
@@ -34,7 +36,10 @@ public class SPARQLController {
 	@RequestMapping(value = MAPPING, method = RequestMethod.POST)	
 	public void postQuery(final HttpServletRequest request,
 			final HttpServletResponse response)
-			throws InterruptedException, ExecutionException, IOException {      	
+			throws InterruptedException, ExecutionException, IOException {
+		String adasfff = request.getContentType();
+		String dsrwerf = request.getHeader("Accept");
+		Enumeration<String> fsdfsdf = request.getHeaderNames();
 		sparqlService.query(request, response, SPARQLService.MIME_SPARQL_RESULTS_JSON);	
 	}
 	
