c7553c699ea311d3f513afac03a7d4bbedf5de36
Bert Spaan <bert.spaan@gmail.com>
2012-04-09 22:05:00 +0200

    Forgot to commit two files...

---
+++ artsholland-core/src/main/java/org/waag/ah/rest/model/SPARQLQuery.java
@@ -0,0 +1,8 @@
+package org.waag.ah.rest.model;
+
+public class SPARQLQuery {
+	
+	private static final String PAGING_PLACEMARK = "[[paging]]";
+	private static final String STATEMENTS_PLACEMARK = "[[statements]]";
+	private static final String FILTER_PLACEMARK = "[[filter]]";
+}
