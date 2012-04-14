3cf186b2e4802ff659cabb31f415d4396bb5f6ad
raoul <raoul@waag.org>
2012-04-14 15:10:43 +0200

    Added authentication for REST & API keys for betausers

--- artsholland-api/src/main/java/org/waag/ah/spring/controller/RestController.java
+++ artsholland-api/src/main/java/org/waag/ah/spring/controller/RestController.java
@@ -10,6 +10,7 @@ import org.openrdf.query.MalformedQueryException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -46,6 +47,7 @@ public class RestController { // implements InitializingBean
 		return null;
 	}
 
+	@Secured("ROLE_API_USER")
 	@RequestMapping(value="/rest/**", method=RequestMethod.GET)
 	public ModelAndView restRequest(
 			final HttpServletRequest request,
