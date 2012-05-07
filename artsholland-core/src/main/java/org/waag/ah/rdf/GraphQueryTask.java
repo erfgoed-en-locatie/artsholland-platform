246d3f64d2174fd17b140a9499dc539a0d97d6cb
raoul <raoul@waag.org>
2012-05-07 17:12:58 +0200

    Fixed issue with rdf/xml return type

--- artsholland-core/src/main/java/org/waag/ah/rdf/GraphQueryTask.java
+++ artsholland-core/src/main/java/org/waag/ah/rdf/GraphQueryTask.java
@@ -7,11 +7,15 @@ import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFWriter;
 import org.openrdf.rio.RDFWriterRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.waag.ah.QueryDefinition;
 import org.waag.ah.RepositoryConnectionFactory;
 import org.waag.ah.WriterConfig;
 
 public class GraphQueryTask extends AbstractQueryTask {
+	final static Logger logger = LoggerFactory.getLogger(GraphQueryTask.class);
+
 	public GraphQueryTask(RepositoryConnectionFactory cf,
 			QueryDefinition query, WriterConfig config, OutputStream os) {
 		super(cf, query, config, os);
@@ -23,11 +27,12 @@ public class GraphQueryTask extends AbstractQueryTask {
 		final GraphQuery sailGraphQuery = cxn
 				.prepareGraphQuery(query.getQueryLanguage(), query.getQuery(),
 						config.getBaseUri());
-//		RDFFormat.forMIMEType(config.getFormat(), RDFFormat.RDFXML)
-		final RDFFormat format = RDFWriterRegistry.getInstance()
-				.getFileFormatForMIMEType(config.getFormat());
+		final RDFFormat format = RDFFormat.forMIMEType(config.getFormat(),
+				RDFFormat.RDFXML);
+		// final RDFFormat format = RDFWriterRegistry.getInstance()
+		// .getFileFormatForMIMEType(config.getFormat());
 		final RDFWriter w = RDFWriterRegistry.getInstance().get(format)
-				.getWriter(os);	
+				.getWriter(os);
 		applyConfig(w);
 		sailGraphQuery.evaluate(w);
 	}
