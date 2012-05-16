c45b4ac4cc6ed11e0dc4f2fcd8a49709659b182b
raoul <raoul@waag.org>
2012-05-16 11:49:58 +0200

    Updated exception handling

--- artsholland-core/src/main/java/org/waag/ah/QueryTask.java
+++ artsholland-core/src/main/java/org/waag/ah/QueryTask.java
@@ -2,6 +2,8 @@ package org.waag.ah;
 
 import java.util.concurrent.Callable;
 
+import org.openrdf.query.MalformedQueryException;
+
 public interface QueryTask extends Callable<Void> {
-	long getCount() throws UnsupportedOperationException;
+	long getCount() throws UnsupportedOperationException, MalformedQueryException;
 }
--- artsholland-core/src/main/java/org/waag/ah/rdf/AbstractQueryTask.java
+++ artsholland-core/src/main/java/org/waag/ah/rdf/AbstractQueryTask.java
@@ -5,6 +5,7 @@ import java.io.OutputStream;
 import org.openrdf.model.Literal;
 import org.openrdf.model.Value;
 import org.openrdf.query.BindingSet;
+import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResult;
@@ -20,7 +21,7 @@ abstract class AbstractQueryTask implements QueryTask {
 	private OutputStream os;
 	private final QueryDefinition query;
 	private final WriterConfig config;
-	
+
 	public AbstractQueryTask(RepositoryConnection conn, QueryDefinition query,
 			WriterConfig config, OutputStream os) {
 		this.conn = conn;
@@ -28,12 +29,13 @@ abstract class AbstractQueryTask implements QueryTask {
 		this.config = config;
 		this.os = os;
 	}
-	
+
 	@Override
-	public long getCount() throws UnsupportedOperationException {
+	public long getCount() throws UnsupportedOperationException,
+			MalformedQueryException {
 		String countQuery = query.getCountQuery();
 		if (countQuery == null) {
-			throw new UnsupportedOperationException("No count qwuery specified");
+			throw new UnsupportedOperationException("No count query specified");
 		}
 		try {
 			TupleQuery tupleQuery = conn.prepareTupleQuery(
@@ -49,6 +51,8 @@ abstract class AbstractQueryTask implements QueryTask {
 				}
 			}
 			return 0;
+		} catch (MalformedQueryException e) {
+			throw e;
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -72,123 +76,124 @@ abstract class AbstractQueryTask implements QueryTask {
 		}
 		return null;
 	}
-	
+
 	protected void applyConfig(RDFWriter writer) {
 		if (ConfigurableRDFWriter.class.isAssignableFrom(writer.getClass())) {
 			((ConfigurableRDFWriter) writer).setConfig(config);
 		}
 	}
-	
+
 	protected abstract void doQuery(QueryDefinition query, WriterConfig config,
 			RepositoryConnection cxn, OutputStream os) throws Exception;
-	
-//	protected BigdataSailQuery buildQuery() {
-//		AbstractQuery query = final AbstractQuery query = newQuery(cxn);
-//		applyBindings(query);
-//		return newQuery(cxn);		
-//	}
-//    private SailQuery newQuery(SailConnection cxn) {
 
-//      final ASTContainer astContainer = ((BigdataParsedQuery) parsedQuery)
-//              .getASTContainer();
+	// protected BigdataSailQuery buildQuery() {
+	// AbstractQuery query = final AbstractQuery query = newQuery(cxn);
+	// applyBindings(query);
+	// return newQuery(cxn);
+	// }
+	// private SailQuery newQuery(SailConnection cxn) {
 
-//      final QueryType queryType = ((BigdataParsedQuery) parsedQuery)
-//              .getQueryType();
+	// final ASTContainer astContainer = ((BigdataParsedQuery) parsedQuery)
+	// .getASTContainer();
 
-//      switch (queryType) {
-//	      case SELECT:
-//	          return new BigdataSailTupleQuery(astContainer, cxn);
-//	      case DESCRIBE:
-//	      case CONSTRUCT:
-////	          return new BigdataSailGraphQuery(astContainer, cxn);
-//	    	  TupleQuery tupleQuery = cxn.p
-//	          return new SailGraphQuery(cxn);
-//	      case ASK: {
-//	          return new BigdataSailBooleanQuery(astContainer, cxn);
-//	      }
-//	      default:
-//	          throw new RuntimeException("Unknown query type: " + queryType);
-//      }
-//    }
-	
-//	public AbstractBigdataQueryTask(String namespace, long timestamp,
-//			String baseURI, ASTContainer astContainer, QueryType queryType,
-//			String defaultMIMEType, Charset charset, String defaultFileExtension,
-//			OutputStream os, RDFWriterConfig config) {
-//		super(namespace, timestamp, baseURI, astContainer, queryType,
-//				defaultMIMEType, charset, defaultFileExtension,
-//				new MockHttpServletRequest(), os);
-//		this.config = config;
-//	}
+	// final QueryType queryType = ((BigdataParsedQuery) parsedQuery)
+	// .getQueryType();
 
-//	public AbstractQueryTask(final String mimeType) {
-//		this.mimeType = mimeType;
-//	}
-//
-//	abstract protected void doQuery(BigdataSailRepositoryConnection cxn,
-//            OutputStream os) throws Exception;
-//
-//	public void setBinding(String key, Value value) {
-//		bindings.put(key, value);
-//	}
-//
-//	protected AbstractQuery buildQuery(
-//			final BigdataSailRepositoryConnection cxn) {
-//		AbstractQuery query = setupQuery(cxn);
-//		applyBindings(query);
-//		return query;
-//	}
-//	
-//    final AbstractQuery setupQuery(final BigdataSailRepositoryConnection cxn) {
-//
-//        // Note the begin time for the query.
-//        final long begin =  System.nanoTime();
-//        
-//        final AbstractQuery query = newQuery(cxn);
-//
-//    	// Figure out the UUID under which the query will execute.
-//        final UUID queryId2 = setQueryId(((BigdataSailQuery) query)
-//                .getASTContainer());
-//        
-//        // Override query if data set protocol parameters were used.
-//		overrideDataset(query);
-//
-//        if (analytic != null) {
-//
-//            // Turn analytic query on/off as requested.
-////            astContainer.getOriginalAST().setQueryHint(QueryHints.ANALYTIC,
-////                    analytic.toString());
-//            astContainer.setQueryHint(QueryHints.ANALYTIC,
-//                    analytic.toString());
-//            
-//        }
-//
-//		// Set the query object.
-//		this.sailQueryOrUpdate = query;
-//		
-//		// Set the IRunningQuery's UUID (volatile write!) 
-//		this.queryId2 = queryId2;
-//		
-//		// Stuff it in the map of running queries.
-//        m_queries.put(queryId, new RunningQuery(queryId.longValue(),
-//                queryId2, begin, this));
-//
-//        return query;
-//        
-//    }
-//    
-//	private void applyBindings(AbstractQuery query) {
-//		if (bindings.size() == 0) {
-//			return;
-//		}
-//		for (Entry<String, Value> binding : bindings.entrySet()) {
-//			query.setBinding(binding.getKey(), binding.getValue());
-//		}
-//	}
-//	
-//	protected void applyConfig(RDFWriter writer) {
-//		if (ConfigurableRDFWriter.class.isAssignableFrom(writer.getClass())) {
-//			((ConfigurableRDFWriter) writer).setConfig(config);
-//		}
-//	}
+	// switch (queryType) {
+	// case SELECT:
+	// return new BigdataSailTupleQuery(astContainer, cxn);
+	// case DESCRIBE:
+	// case CONSTRUCT:
+	// // return new BigdataSailGraphQuery(astContainer, cxn);
+	// TupleQuery tupleQuery = cxn.p
+	// return new SailGraphQuery(cxn);
+	// case ASK: {
+	// return new BigdataSailBooleanQuery(astContainer, cxn);
+	// }
+	// default:
+	// throw new RuntimeException("Unknown query type: " + queryType);
+	// }
+	// }
+
+	// public AbstractBigdataQueryTask(String namespace, long timestamp,
+	// String baseURI, ASTContainer astContainer, QueryType queryType,
+	// String defaultMIMEType, Charset charset, String defaultFileExtension,
+	// OutputStream os, RDFWriterConfig config) {
+	// super(namespace, timestamp, baseURI, astContainer, queryType,
+	// defaultMIMEType, charset, defaultFileExtension,
+	// new MockHttpServletRequest(), os);
+	// this.config = config;
+	// }
+
+	// public AbstractQueryTask(final String mimeType) {
+	// this.mimeType = mimeType;
+	// }
+	//
+	// abstract protected void doQuery(BigdataSailRepositoryConnection cxn,
+	// OutputStream os) throws Exception;
+	//
+	// public void setBinding(String key, Value value) {
+	// bindings.put(key, value);
+	// }
+	//
+	// protected AbstractQuery buildQuery(
+	// final BigdataSailRepositoryConnection cxn) {
+	// AbstractQuery query = setupQuery(cxn);
+	// applyBindings(query);
+	// return query;
+	// }
+	//
+	// final AbstractQuery setupQuery(final BigdataSailRepositoryConnection cxn)
+	// {
+	//
+	// // Note the begin time for the query.
+	// final long begin = System.nanoTime();
+	//
+	// final AbstractQuery query = newQuery(cxn);
+	//
+	// // Figure out the UUID under which the query will execute.
+	// final UUID queryId2 = setQueryId(((BigdataSailQuery) query)
+	// .getASTContainer());
+	//
+	// // Override query if data set protocol parameters were used.
+	// overrideDataset(query);
+	//
+	// if (analytic != null) {
+	//
+	// // Turn analytic query on/off as requested.
+	// // astContainer.getOriginalAST().setQueryHint(QueryHints.ANALYTIC,
+	// // analytic.toString());
+	// astContainer.setQueryHint(QueryHints.ANALYTIC,
+	// analytic.toString());
+	//
+	// }
+	//
+	// // Set the query object.
+	// this.sailQueryOrUpdate = query;
+	//
+	// // Set the IRunningQuery's UUID (volatile write!)
+	// this.queryId2 = queryId2;
+	//
+	// // Stuff it in the map of running queries.
+	// m_queries.put(queryId, new RunningQuery(queryId.longValue(),
+	// queryId2, begin, this));
+	//
+	// return query;
+	//
+	// }
+	//
+	// private void applyBindings(AbstractQuery query) {
+	// if (bindings.size() == 0) {
+	// return;
+	// }
+	// for (Entry<String, Value> binding : bindings.entrySet()) {
+	// query.setBinding(binding.getKey(), binding.getValue());
+	// }
+	// }
+	//
+	// protected void applyConfig(RDFWriter writer) {
+	// if (ConfigurableRDFWriter.class.isAssignableFrom(writer.getClass())) {
+	// ((ConfigurableRDFWriter) writer).setConfig(config);
+	// }
+	// }
 }
