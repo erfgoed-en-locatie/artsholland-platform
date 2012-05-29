7fb5ce0a3fc20a8a082a07712c4459b211970e2c
raoul <raoul@waag.org>
2012-05-29 12:19:45 +0200

    Latest changes

--- artsholland-processor/src/main/java/PipesTest.java
+++ artsholland-processor/src/main/java/PipesTest.java
@@ -16,8 +16,13 @@ public class PipesTest {
 
 		List<URL> resources = new ArrayList<URL>(Arrays.asList(
 //			new URL("http://ps4.uitburo.nl/api/search?key=505642b12881b9a60688411a333bc78b&resolve=true&rows=10&start=0&createdto=2012-04-29T13:44:00.007Z")
-//			new URL("http://ps4.uitburo.nl/api/search?key=505642b12881b9a60688411a333bc78b&resolve=true&rows=500&start=0&createdto=2012-04-29T13:44:00.007Z"),
-			new URL("http://ps4.uitburo.nl/api/search?key=505642b12881b9a60688411a333bc78b&resolve=true&rows=500&start=500&createdto=2012-04-29T13:44:00.007Z")
+			new URL("http://ps4.uitburo.nl/api/search?key=505642b12881b9a60688411a333bc78b&resolve=true&rows=500&start=0&createdto=2012-04-29T13:44:00.007Z"),
+			new URL("http://ps4.uitburo.nl/api/search?key=505642b12881b9a60688411a333bc78b&resolve=true&rows=500&start=500&createdto=2012-04-29T13:44:00.007Z"),
+			new URL("http://ps4.uitburo.nl/api/search?key=505642b12881b9a60688411a333bc78b&resolve=true&rows=500&start=1000&createdto=2012-04-29T13:44:00.007Z"),
+			new URL("http://ps4.uitburo.nl/api/search?key=505642b12881b9a60688411a333bc78b&resolve=true&rows=500&start=1500&createdto=2012-04-29T13:44:00.007Z"),
+			new URL("http://ps4.uitburo.nl/api/search?key=505642b12881b9a60688411a333bc78b&resolve=true&rows=500&start=2000&createdto=2012-04-29T13:44:00.007Z"),
+			new URL("http://ps4.uitburo.nl/api/search?key=505642b12881b9a60688411a333bc78b&resolve=true&rows=500&start=2500&createdto=2012-04-29T13:44:00.007Z"),
+			new URL("http://ps4.uitburo.nl/api/search?key=505642b12881b9a60688411a333bc78b&resolve=true&rows=500&start=3000&createdto=2012-04-29T13:44:00.007Z")
 		));
 		
 		// Assemble pipeline.
