8062bfdcc211e56efd6d0c7880298157e3bbed3f
Bert Spaan <bert.spaan@gmail.com>
2012-06-27 13:53:19 +0200

    Reads homepage predicate

--- artsholland-processor/src/main/java/org/waag/ah/tinkerpop/enrich/DBpediaEnricher.java
+++ artsholland-processor/src/main/java/org/waag/ah/tinkerpop/enrich/DBpediaEnricher.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.configuration.ConfigurationException;
+import org.openrdf.model.Literal;
+import org.openrdf.model.Resource;
 import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
 import org.openrdf.model.ValueFactory;
 import org.openrdf.model.util.GraphUtil;
 import org.waag.ah.rdf.EnricherConfig;
@@ -23,7 +26,15 @@ public class DBpediaEnricher extends AbstractEnricher {
 		ValueFactory vf = graph.getValueFactory();
 		
 		try {
-			String homepage = GraphUtil.getUniqueObjectLiteral(graph, null, vf.createURI("http://xmlns.com/foaf/0.1/homepage")).stringValue();
+			
+			//Facebook
+			//SELECT name, website, page_url, fan_count, location FROM page WHERE  name = "Gelredome"			
+			//Foursquare (ook twitter-naam)
+			//https://developer.foursquare.com/docs/explore#req=venues/search%3Fquery%3Drijksmuseum%26ll%3D52.356,4.825+
+			
+			Resource vio = GraphUtil.getOptionalObjectResource(graph, null, vf.createURI("http://xmlns.com/foaf/0.1/homepage"));
+			
+			//String homepage = GraphUtil.getUniqueObjectLiteral(graph, null, vf.createURI("http://xmlns.com/foaf/0.1/homepage")).stringValue();
 
 			String sparql = "SELECT ?resource WHERE { ?resource foaf:homepage <http://www.rijksmuseum.nl/> .	}";
 			
