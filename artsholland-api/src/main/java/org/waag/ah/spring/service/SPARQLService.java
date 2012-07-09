0dd98a7370f98bf43b84fc77d03f296c9432a4aa
raoul <raoul@waag.org>
2012-07-09 19:26:15 +0200

    Some BigData optimizations

--- artsholland-api/src/main/java/org/waag/ah/spring/service/SPARQLService.java
+++ artsholland-api/src/main/java/org/waag/ah/spring/service/SPARQLService.java
@@ -1,6 +1,7 @@
 package org.waag.ah.spring.service;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -13,26 +14,42 @@ import java.util.concurrent.TimeoutException;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.parser.ParsedBooleanQuery;
+import org.openrdf.query.parser.ParsedGraphQuery;
+import org.openrdf.query.parser.ParsedQuery;
+import org.openrdf.query.parser.ParsedTupleQuery;
+import org.openrdf.query.parser.QueryParser;
+import org.openrdf.query.parser.sparql.SPARQLParserFactory;
+import org.openrdf.repository.RepositoryConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.waag.ah.QueryService;
+import org.waag.ah.QueryDefinition;
 import org.waag.ah.QueryTask;
+import org.waag.ah.RepositoryConnectionFactory;
+import org.waag.ah.WriterConfig;
+import org.waag.ah.rdf.AskQueryTask;
+import org.waag.ah.rdf.GraphQueryTask;
 import org.waag.ah.rdf.RDFWriterConfig;
 import org.waag.ah.rdf.RdfQueryDefinition;
+import org.waag.ah.rdf.TupleQueryTask;
 import org.waag.ah.rest.util.MIMEParse;
 
 @Service(value="sparqlService")
 public class SPARQLService {
 	private static final Logger logger = LoggerFactory.getLogger(SPARQLService.class);
 	
-	@EJB(mappedName="java:app/datastore/BigdataQueryService")
-	private QueryService context;
+	@EJB(mappedName="java:app/datastore/BigdataConnectionService")
+	private RepositoryConnectionFactory cf;
+	
+//	@EJB(mappedName="java:app/datastore/BigdataQueryService")
+//	private QueryService context;
 
 	private ExecutorService executor;
 
@@ -86,8 +103,8 @@ public class SPARQLService {
 			RdfQueryDefinition query = new RdfQueryDefinition(
 					QueryLanguage.SPARQL, request.getParameter("query"));
         	
-			final QueryTask queryTask = context.getQueryTask(query, config,
-					response.getOutputStream());
+			ServletOutputStream out = response.getOutputStream();
+			final QueryTask queryTask = getQueryTask(query, config, out);
             response.setStatus(HttpServletResponse.SC_OK);
 
             Future<Void> ft = executor.submit(queryTask);
@@ -107,6 +124,32 @@ public class SPARQLService {
 //			e.getCause().printStackTrace();
 			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
 					e.getCause().getMessage());
+		} finally {
+			
 		}
 	}
+	
+//	@Override
+	public QueryTask getQueryTask(QueryDefinition query,
+			WriterConfig config, OutputStream out)
+			throws MalformedQueryException {
+		QueryParser parser = new SPARQLParserFactory().getParser();
+		ParsedQuery parsedQuery = parser.parseQuery(query.getQuery(),
+				config.getBaseUri());
+		try {
+			RepositoryConnection conn = cf.getConnection();
+			if (parsedQuery instanceof ParsedTupleQuery) {
+				return new TupleQueryTask(conn, query, config, out);
+			} else if (parsedQuery instanceof ParsedBooleanQuery) {
+				return new AskQueryTask(conn, query, config, out);
+			} else if (parsedQuery instanceof ParsedGraphQuery) {
+				return new GraphQueryTask(conn, query, config, out);
+			}			
+		} catch (Exception e) {
+			throw new RuntimeException(e.getMessage(), e);
+		}
+		
+		throw new MalformedQueryException("Unknown query type: "
+				+ ParsedQuery.class.getName());
+	}	
 }
--- artsholland-datastore/src/main/java/org/waag/ah/bigdata/BigdataQueryService.java
+++ artsholland-datastore/src/main/java/org/waag/ah/bigdata/BigdataQueryService.java
@@ -1,58 +1,60 @@
-package org.waag.ah.bigdata;
-
-import java.io.OutputStream;
-
-import javax.ejb.DependsOn;
-import javax.ejb.EJB;
-import javax.ejb.Singleton;
-
-import org.openrdf.query.MalformedQueryException;
-import org.openrdf.query.parser.ParsedBooleanQuery;
-import org.openrdf.query.parser.ParsedGraphQuery;
-import org.openrdf.query.parser.ParsedQuery;
-import org.openrdf.query.parser.ParsedTupleQuery;
-import org.openrdf.query.parser.QueryParser;
-import org.openrdf.query.parser.sparql.SPARQLParserFactory;
-import org.openrdf.repository.RepositoryConnection;
-import org.waag.ah.QueryDefinition;
-import org.waag.ah.QueryService;
-import org.waag.ah.QueryTask;
-import org.waag.ah.RepositoryConnectionFactory;
-import org.waag.ah.WriterConfig;
-import org.waag.ah.rdf.AskQueryTask;
-import org.waag.ah.rdf.GraphQueryTask;
-import org.waag.ah.rdf.TupleQueryTask;
-
-@Singleton
-@DependsOn("BigdataConnectionService")
-public class BigdataQueryService implements QueryService {
+//package org.waag.ah.bigdata;
+//
+//import java.io.OutputStream;
+//
+//import javax.ejb.DependsOn;
+//import javax.ejb.EJB;
+//import javax.ejb.Singleton;
+//
+//import org.openrdf.query.MalformedQueryException;
+//import org.openrdf.query.parser.ParsedBooleanQuery;
+//import org.openrdf.query.parser.ParsedGraphQuery;
+//import org.openrdf.query.parser.ParsedQuery;
+//import org.openrdf.query.parser.ParsedTupleQuery;
+//import org.openrdf.query.parser.QueryParser;
+//import org.openrdf.query.parser.sparql.SPARQLParserFactory;
+//import org.openrdf.repository.RepositoryConnection;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.waag.ah.QueryDefinition;
+//import org.waag.ah.QueryService;
+//import org.waag.ah.QueryTask;
+//import org.waag.ah.RepositoryConnectionFactory;
+//import org.waag.ah.WriterConfig;
+//import org.waag.ah.rdf.AskQueryTask;
+//import org.waag.ah.rdf.GraphQueryTask;
+//import org.waag.ah.rdf.TupleQueryTask;
+//
+//@Singleton
+//@DependsOn("BigdataConnectionService")
+//public class BigdataQueryService implements QueryService {
 //	private static final Logger logger = LoggerFactory
 //			.getLogger(BigdataQueryService.class);
-	
-	@EJB(mappedName="java:module/BigdataConnectionService")
-	private RepositoryConnectionFactory cf;
-
-	@Override
-	public QueryTask getQueryTask(QueryDefinition query,
-			WriterConfig config, OutputStream out)
-			throws MalformedQueryException {
-		QueryParser parser = new SPARQLParserFactory().getParser();
-		ParsedQuery parsedQuery = parser.parseQuery(query.getQuery(),
-				config.getBaseUri());
-		try {
-			RepositoryConnection conn = cf.getConnection();
-			if (parsedQuery instanceof ParsedTupleQuery) {
-				return new TupleQueryTask(conn, query, config, out);
-			} else if (parsedQuery instanceof ParsedBooleanQuery) {
-				return new AskQueryTask(conn, query, config, out);
-			} else if (parsedQuery instanceof ParsedGraphQuery) {
-				return new GraphQueryTask(conn, query, config, out);
-			}			
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage(), e);
-		}
-		
-		throw new MalformedQueryException("Unknown query type: "
-				+ ParsedQuery.class.getName());
-	}
-}
+//	
+//	@EJB(mappedName="java:module/BigdataConnectionService")
+//	private RepositoryConnectionFactory cf;
+//
+//	@Override
+//	public QueryTask getQueryTask(QueryDefinition query,
+//			WriterConfig config, OutputStream out)
+//			throws MalformedQueryException {
+//		QueryParser parser = new SPARQLParserFactory().getParser();
+//		ParsedQuery parsedQuery = parser.parseQuery(query.getQuery(),
+//				config.getBaseUri());
+//		try {
+//			RepositoryConnection conn = cf.getConnection();
+//			if (parsedQuery instanceof ParsedTupleQuery) {
+//				return new TupleQueryTask(conn, query, config, out);
+//			} else if (parsedQuery instanceof ParsedBooleanQuery) {
+//				return new AskQueryTask(conn, query, config, out);
+//			} else if (parsedQuery instanceof ParsedGraphQuery) {
+//				return new GraphQueryTask(conn, query, config, out);
+//			}			
+//		} catch (Exception e) {
+//			throw new RuntimeException(e.getMessage(), e);
+//		}
+//		
+//		throw new MalformedQueryException("Unknown query type: "
+//				+ ParsedQuery.class.getName());
+//	}
+//}
