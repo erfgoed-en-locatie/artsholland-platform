f69174d28fa73606ce0fcdf3fa5a7f262b8b8681
Bert Spaan <bert.spaan@gmail.com>
2012-07-24 10:49:15 +0200

    Now with Facebook enrichtment!

--- artsholland-processor/src/main/java/org/waag/ah/tinkerpop/enrich/SocialMediaEnricher.java
+++ artsholland-processor/src/main/java/org/waag/ah/tinkerpop/enrich/SocialMediaEnricher.java
@@ -14,6 +14,11 @@ import org.waag.ah.rdf.EnricherConfig;
 import org.waag.ah.rdf.NamedGraph;
 import org.waag.ah.tinkerpop.AbstractEnricher;
 
+import com.restfb.DefaultFacebookClient;
+import com.restfb.Facebook;
+import com.restfb.FacebookClient;
+import com.restfb.types.Location;
+
 import fi.foyt.foursquare.api.FoursquareApi;
 import fi.foyt.foursquare.api.FoursquareApiException;
 import fi.foyt.foursquare.api.Result;
@@ -23,24 +28,51 @@ import fi.foyt.foursquare.api.entities.VenuesSearchResult;
 
 public class SocialMediaEnricher extends AbstractEnricher {
 
+	protected static class FqlPage {
+		@Facebook
+		String page_id;
+		
+		@Facebook
+		String name;
+		
+		@Facebook
+		String website;
+		
+		@Facebook
+		String page_url;
+	
+		@Facebook
+		String fan_count;
+		
+		@Facebook
+		Location location;
+	}	
+	
 	private PlatformConfig platformConfig;
+	
 	private FoursquareApi foursquareApi;
+	private FacebookClient facebookClient;
 	
 	public SocialMediaEnricher(EnricherConfig enricherConfig)
 			throws ConfigurationException {
 		super(enricherConfig);
 		this.platformConfig = PlatformConfigHelper.getConfig();
 		
+		// Foursquare
 		String clientId = platformConfig.getString("foursquare.api.clientId");
 		String clientSecret = platformConfig.getString("foursquare.api.clientSecret");		
 		this.foursquareApi = new FoursquareApi(clientId, clientSecret, "http://dev.artsholland.com");
+		
+		// Facebook
+		//String accessToken = platformConfig.getString("facebook.api.accessToken");
+		//facebookClient = new DefaultFacebookClient(accessToken);
+		facebookClient = new DefaultFacebookClient();
 	}
 
 	@Override
 	public List<Statement> enrich(EnricherConfig config, NamedGraph graph) {
 		List<Statement> statements = new ArrayList<Statement>();
 		ValueFactory vf = graph.getValueFactory();
-
 		String classUri = platformConfig.getString("platform.classUri");
 
 		// Facebook, met FQL:
@@ -70,37 +102,7 @@ public class SocialMediaEnricher extends AbstractEnricher {
 					vf.createURI("http://www.w3.org/2003/01/geo/wgs84_pos#long"))
 					.floatValue();
 
-			Result<VenuesSearchResult> searchResult = foursquareApi.venuesSearch(
-					Float.toString(lat) + "," + Float.toString(lon), null, null, null,
-					null, null, null, null, homepageStr, null, null);
-			if (searchResult.getMeta().getCode() == 200) {
-				// if query was ok we can finally we do something with the data
-				for (CompactVenue venue : searchResult.getResult().getVenues()) {
-					String foursquareId = venue.getId();
-					Contact contact = venue.getContact();
-					String twitterId = contact.getTwitter();
-					String facebookId = contact.getFacebook();
-
-					if (foursquareId != null) {
-						statements.add(vf.createStatement(graph.getGraphUri(),
-								vf.createURI(classUri + "foursquare"),
-								vf.createURI(getFoursquareUrl(foursquareId))));
-					}
-
-					if (twitterId != null) {
-						statements.add(vf.createStatement(graph.getGraphUri(),
-								vf.createURI(classUri + "twitter"),
-								vf.createURI(getTwitterUrl(twitterId))));
-					}
-
-					if (facebookId != null) {
-						statements.add(vf.createStatement(graph.getGraphUri(),
-								vf.createURI(classUri + "facebook"),
-								vf.createURI(getFacebookUrl(facebookId))));
-					}
-
-				}
-			}
+			getFoursquareStatements(graph, statements, vf, classUri, homepageStr, lat, lon);
 		} catch (FoursquareApiException e) {
 			e.printStackTrace();
 		} catch (GraphUtilException e) {
@@ -110,15 +112,78 @@ public class SocialMediaEnricher extends AbstractEnricher {
 		return statements;
 	}
 
-	private String getFoursquareUrl(String foursquareId) {
-		return "http://foursquare.com/venue/" + foursquareId;
+	protected void getFacebookStatements(NamedGraph graph, List<Statement> statements,
+			ValueFactory vf, String classUri, String homepageStr, float lat, float lon, String name, String city) {		
+		
+		String query = "SELECT page_id, name, website, page_url, fan_count, location FROM page WHERE name = \"%s\"";		
+		List<FqlPage> pages = facebookClient.executeQuery(String.format(query, name), FqlPage.class);
+
+		if (pages.size() >= 1) {
+			FqlPage page = pages.get(0);
+			String facebookCity = page.location.getCity();
+			if (facebookCity != null && facebookCity.equals(city)) {			
+				String pageUrl = page.page_url;
+				String pageId = page.page_id;			
+				
+				statements.add(vf.createStatement(graph.getGraphUri(),
+						vf.createURI(classUri + "facebookPageId"), 
+						vf.createLiteral(pageId)));
+				statements.add(vf.createStatement(graph.getGraphUri(),
+						vf.createURI(classUri + "facebookUrl"), 
+						vf.createURI(pageUrl)));
+			}
+		}
+	}
+	
+	protected void getFoursquareStatements(NamedGraph graph, List<Statement> statements,
+			ValueFactory vf, String classUri, String homepageStr, float lat, float lon)
+			throws FoursquareApiException {
+		
+		Result<VenuesSearchResult> searchResult = foursquareApi.venuesSearch(
+				Float.toString(lat) + "," + Float.toString(lon), null, null, null,
+				null, null, null, null, homepageStr, null, null);
+		
+		if (searchResult.getMeta().getCode() == 200) {
+			// if query was ok we can finally we do something with the data
+			for (CompactVenue venue : searchResult.getResult().getVenues()) {
+				
+				String name = venue.getName();
+				String city = venue.getLocation().getCity();
+				getFacebookStatements(graph, statements, vf, classUri, homepageStr, lat, lon, name, city);
+				
+				String foursquareId = venue.getId();
+				Contact contact = venue.getContact();
+				String twitterId = contact.getTwitter();
+				
+				if (foursquareId != null) {
+					statements.add(vf.createStatement(graph.getGraphUri(),
+							vf.createURI(classUri + "foursquareId"), 
+							vf.createLiteral(foursquareId)));
+					
+					statements.add(vf.createStatement(graph.getGraphUri(),
+							vf.createURI(classUri + "foursquareUrl"), 
+							vf.createURI(getFoursquareUrl(foursquareId))));
+				}
+
+				if (twitterId != null) {
+					statements.add(vf.createStatement(graph.getGraphUri(),
+							vf.createURI(classUri + "twitterId"),
+							vf.createURI(twitterId)));
+					
+					statements.add(vf.createStatement(graph.getGraphUri(),
+							vf.createURI(classUri + "twitterUrl"),
+							vf.createURI(getTwitterUrl(twitterId))));
+				}
+			}
+		}
 	}
 
 	private String getTwitterUrl(String twitterId) {
-		return "http://twitter.com/" + twitterId;
+		return "https://twitter.com/" + twitterId;
 	}
 
-	private String getFacebookUrl(String facebookId) {
-		return "http://facebook.com/" + facebookId;
+	private String getFoursquareUrl(String foursquareId) {
+		return "https://foursquare.com/v/" + foursquareId;
 	}
+	
 }
