ace42a86ecdd9de5aff855357a3956736a605632
Bert Spaan <bert.spaan@gmail.com>
2012-08-02 11:16:56 +0200

    ..

--- artsholland-api/src/main/java/org/waag/ah/spring/dao/UserDAOImpl.java
+++ artsholland-api/src/main/java/org/waag/ah/spring/dao/UserDAOImpl.java
@@ -2,6 +2,7 @@ package org.waag.ah.spring.dao;
 
 import java.util.List;
 
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
