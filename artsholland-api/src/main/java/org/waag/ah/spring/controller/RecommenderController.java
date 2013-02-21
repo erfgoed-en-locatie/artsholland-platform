c2f8de205fcea2472c7588949e73dbc24de828bf
raoul <raoul@waag.org>
2013-02-21 13:50:14 +0100

    Temporalily disabled recommender

--- artsholland-api/src/main/java/org/waag/ah/spring/controller/RecommenderController.java
+++ artsholland-api/src/main/java/org/waag/ah/spring/controller/RecommenderController.java
@@ -1,50 +1,50 @@
-package org.waag.ah.spring.controller;
-
-import java.io.IOException;
-import java.util.List;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.waag.ah.rest.model.RestProfile;
-import org.waag.ah.spring.service.ProfileService;
-import org.waag.ah.spring.service.RecommenderService;
-
-import com.google.gson.Gson;
-
-@Controller
-@RequestMapping(value="/recommend")
-public class RecommenderController {
-	final Logger logger = LoggerFactory.getLogger(RecommenderController.class);
-	
-	@Resource(name="profileService")
-	private ProfileService profileService;
-	
-	@Autowired RecommenderService recommenderService;
-	
-	@Secured("ROLE_API_USER")
-	@RequestMapping(method=RequestMethod.GET)
-	public @ResponseBody String restRequest(
-			final HttpServletRequest request,
-			final HttpServletResponse response,
-			final @RequestParam("profile_id") String profileId)
-			throws IOException {
-		RestProfile profile = profileService.getProfileById(profileId);
-		List<String> recommendations = recommenderService.recommendForProfile(profile);
-//		ModelAndView view = new ModelAndView();
-//		view.addObject(recommendations);
-//		return view;
-		Gson gson = new Gson();
-		return gson.toJson(recommendations);
-	}
-}
+//package org.waag.ah.spring.controller;
+//
+//import java.io.IOException;
+//import java.util.List;
+//
+//import javax.annotation.Resource;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.access.annotation.Secured;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.waag.ah.rest.model.RestProfile;
+//import org.waag.ah.spring.service.ProfileService;
+//import org.waag.ah.spring.service.RecommenderService;
+//
+//import com.google.gson.Gson;
+//
+//@Controller
+//@RequestMapping(value="/recommend")
+//public class RecommenderController {
+//	final Logger logger = LoggerFactory.getLogger(RecommenderController.class);
+//	
+//	@Resource(name="profileService")
+//	private ProfileService profileService;
+//	
+//	@Autowired RecommenderService recommenderService;
+//	
+//	@Secured("ROLE_API_USER")
+//	@RequestMapping(method=RequestMethod.GET)
+//	public @ResponseBody String restRequest(
+//			final HttpServletRequest request,
+//			final HttpServletResponse response,
+//			final @RequestParam("profile_id") String profileId)
+//			throws IOException {
+//		RestProfile profile = profileService.getProfileById(profileId);
+//		List<String> recommendations = recommenderService.recommendForProfile(profile);
+////		ModelAndView view = new ModelAndView();
+////		view.addObject(recommendations);
+////		return view;
+//		Gson gson = new Gson();
+//		return gson.toJson(recommendations);
+//	}
+//}
