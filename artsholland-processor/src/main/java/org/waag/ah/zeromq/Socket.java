016a2fb18c5f91f32e701bf8574a384f1550f5af
raoul <raoul@waag.org>
2013-02-26 22:42:02 +0100

    Updated to JSON messages

--- artsholland-processor/src/main/java/org/waag/ah/zeromq/Socket.java
+++ artsholland-processor/src/main/java/org/waag/ah/zeromq/Socket.java
@@ -1,5 +1,7 @@
 package org.waag.ah.zeromq;
 
 public interface Socket {
-	final String FETCH_URL = "tcp://localhost:5557";
+	final String FETCHER_CLIENT_URL = "tcp://localhost:5557";
+	final String FETCHER_SERVER_URL = "tcp://*:5557";
+	final String FETCHER = "tcp://localhost:5558"; 
 }
