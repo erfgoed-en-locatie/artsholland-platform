27d87dece4f2982dedc05a848a1193ee8e6b3d98
Bert Spaan <bert.spaan@gmail.com>
2013-08-05 16:38:46 +0200

    saves reindex job execution state in mongo

--- artsholland-processor/src/main/java/org/waag/ah/quartz/ReindexJob.java
+++ artsholland-processor/src/main/java/org/waag/ah/quartz/ReindexJob.java
@@ -11,25 +11,44 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.SchedulerException;
-import org.waag.ah.exception.ConnectionException;
-import org.waag.ah.service.RepositoryConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.waag.ah.exception.ConnectionException;
+import org.waag.ah.importer.ImportResult;
+import org.waag.ah.service.MongoConnectionService;
+import org.waag.ah.service.RepositoryConnectionFactory;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 
 public class ReindexJob implements Job {
 	private static final Logger logger = LoggerFactory.getLogger(ReindexJob.class);
 
 	private RepositoryConnectionFactory cf;
+	private DBCollection coll;
 	
 	public ReindexJob() throws NamingException, ConnectionException, RepositoryException {
 		InitialContext ic = new InitialContext();
 		cf = (RepositoryConnectionFactory) ic
 				.lookup("java:global/artsholland-platform/core/RepositoryConnectionService");
+		
+		MongoConnectionService mongo = (MongoConnectionService) ic
+				.lookup("java:global/artsholland-platform/datastore/MongoConnectionServiceImpl");
+		this.coll = mongo.getCollection(UrlImportJob.class.getName());
+		coll.setObjectClass(ImportResult.class);
 	}
 
 	@Override
 	public void execute(JobExecutionContext context)
 			throws JobExecutionException {
+		
+		String jobKey = context.getJobDetail().getKey().toString();
+		ImportResult lastResult = getLastResult(jobKey);
+		if (lastResult != null) {
+			context.setResult("Import job "+jobKey+" with strategy ONCE already executed, skipping.");
+			return;
+		}
 		
 		try {
 			// Execute only once. Unschedule after first execution:
@@ -54,4 +73,20 @@ public class ReindexJob implements Job {
 			logger.info("Unscheduling failed...");
 		}
 	}
+	
+	private ImportResult getLastResult(String jobKey) {
+		BasicDBObject query = new BasicDBObject();
+		query.put("jobKey", jobKey);
+		query.put("success", true);
+
+		DBCursor cur = coll.find(query)
+				.sort(new BasicDBObject("timestamp", -1));
+
+		if (cur.hasNext()) {
+			return (ImportResult) cur.next();
+		}
+		
+		return null;
+	}
+	
 }
